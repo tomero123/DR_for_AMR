ddc17c3da4c27acc2ee4d950287752326f7438db
tomer.orgad <tomer.orgad@biocatch.com>
2019-04-16 21:20:03 +0300

    download assembly_status script

--- ncbi_data/combine_kmers_files.py
+++ ncbi_data/combine_kmers_files.py
@@ -13,7 +13,9 @@ n_of_files = len(files_list)
 
 if __name__ == '__main__':
     all_kmers_dic = {}
+    mapping_dic = {}
     for ind, file_name in enumerate(files_list):
+        mapping_dic[ind] = file_name
         print(f"Started processing: {file_name}")
         with gzip.open(path + input_folder + file_name, "rt") as f:
             kmers_dic = json.loads(f.read())
@@ -25,3 +27,5 @@ if __name__ == '__main__':
                     all_kmers_dic[kmer][ind] = kmers_dic[kmer]
     with gzip.open(path + "all_kmers_file.txt.gz", 'wt') as outfile:
         json.dump(all_kmers_dic, outfile)
+    with open(path + "all_kmers_map.txt", 'wt') as outfile2:
+        json.dump(mapping_dic, outfile2)
