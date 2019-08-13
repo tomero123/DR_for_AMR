942423eac1edbe28f4dfaef48ac60a549a98936e
TomerOrgad88 <tomerorgad@gmail.com>
2019-08-13 16:02:11 +0300

    --

--- ncbi_data/create_all_kmers_csv.py
+++ ncbi_data/create_all_kmers_csv.py
@@ -6,12 +6,16 @@ import pandas as pd
 kmers_file_name = 'all_kmers_file_SMALL.txt.gz'
 kmers_map_file_name = 'all_kmers_map_SMALL.txt'
 
-with gzip.open(os.path.join('..', 'results_files', kmers_file_name), 'rt') as f:
-    all_kmers_dic = json.loads(f.read())
+import glob
+print(glob.glob(os.path.join('..', 'results_files', '*')))
 
-with open(os.path.join('..', 'results_files', kmers_map_file_name), 'r') as f:
-    all_kmers_map = json.loads(f.read())
 
-df = pd.DataFrame({key: pd.Series(val) for key, val in all_kmers_dic.items()})
-df = df.T
-df.to_csv(os.path.join('..', 'results_files', kmers_file_name.replace("txt.gz", "csv")))
+# with gzip.open(os.path.join('..', 'results_files', kmers_file_name), 'rt') as f:
+#     all_kmers_dic = json.loads(f.read())
+#
+# with open(os.path.join('..', 'results_files', kmers_map_file_name), 'r') as f:
+#     all_kmers_map = json.loads(f.read())
+#
+# df = pd.DataFrame({key: pd.Series(val) for key, val in all_kmers_dic.items()})
+# df = df.T
+# df.to_csv(os.path.join('..', 'results_files', kmers_file_name.replace("txt.gz", "csv")))
