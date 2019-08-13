85c3d5e7de7418cc698279e390eb9e15dd26bae5
TomerOrgad88 <tomerorgad@gmail.com>
2019-08-13 16:12:02 +0300

    --

--- ncbi_data/create_all_kmers_csv.py
+++ ncbi_data/create_all_kmers_csv.py
@@ -11,12 +11,12 @@ if os.name == 'nt':
 else:
     path = os.path.join('.', 'results_files')
 
-with gzip.open(path + kmers_file_name, 'rt') as f:
+with gzip.open(os.path.join(path, kmers_file_name), 'rt') as f:
     all_kmers_dic = json.loads(f.read())
 
-with open(path + kmers_map_file_name, 'r') as f:
+with open(os.path.join(path, kmers_map_file_name), 'r') as f:
     all_kmers_map = json.loads(f.read())
 
 df = pd.DataFrame({key: pd.Series(val) for key, val in all_kmers_dic.items()})
 df = df.T
-df.to_csv(path + kmers_file_name.replace("txt.gz", "csv"))
+df.to_csv(os.path.join(path, kmers_file_name.replace("txt.gz", "csv")))
