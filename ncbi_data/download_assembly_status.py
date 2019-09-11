b5ea655ea8b5e3000f49cf6a86f824d7090d6442
TomerOrgad88 <tomerorgad@gmail.com>
2019-09-11 13:09:23 +0300

    --

--- ncbi_data/download_assembly_status.py
+++ ncbi_data/download_assembly_status.py
@@ -1,18 +1,25 @@
+import os
 import pandas as pd
 from pathos.multiprocessing import ProcessPool
 from ncbi_data.ncbi_utils import open_ftp_file
 
 # PARAMS
+BACTERIA = "mycobacterium_tuberculosis"
 NUM_OF_PROCESSES = 8
-DEST_PATH = "C:/University 2nd degree/Thesis/Pseudomonas Aureginosa data/"
-CSV_FILE_PATH = "../data_files/genomes_proks.csv"
+
+PREFIX = '..' if os.name == 'nt' else '.'
+PATH = os.path.join(PREFIX, "data_files", BACTERIA)
+CSV_INPUT_FILE_PATH = os.path.join(PATH, "genomes_proks166.csv")
+CSV_OUTPUT_FILE_PATH = os.path.join(PATH, "{}_data.csv".format(BACTERIA))
+
 NCBI_FTP_SITE = "ftp.ncbi.nlm.nih.gov"
 FTP_FILE_NAME = "assembly_status.txt"
+
+limit = None  # if None - take all files found else limit
 # PARAMS END
 
-
 if __name__ == '__main__':
-    df = pd.read_csv(CSV_FILE_PATH)
+    df = pd.read_csv(CSV_INPUT_FILE_PATH)
     n_rows = df.shape[0]
     input_list = []
     for ind in range(n_rows):
@@ -20,7 +27,8 @@ if __name__ == '__main__':
         ftp_sub_folder = df["RefSeq FTP"][ind][folder_ind:]
         strain_name = df["Strain"][ind]
         input_list.append([ind, ftp_sub_folder, strain_name, FTP_FILE_NAME, NCBI_FTP_SITE])
-    # input_list = input_list[0:20]
+    if limit is not None:
+        input_list = input_list[:limit]
     if NUM_OF_PROCESSES > 1:
         pool = ProcessPool(processes=NUM_OF_PROCESSES)
         status_list = pool.map(open_ftp_file, input_list)
@@ -31,4 +39,4 @@ if __name__ == '__main__':
     # print(status_list)
     status_df = pd.DataFrame({"strain_validation": [x[0] for x in status_list], "status": [x[1].replace("status=", "") for x in status_list]})
     df = pd.concat([df, status_df], axis=1)
-    df.to_csv("../data_files/pa_data.csv", index=False)
+    df.to_csv(CSV_OUTPUT_FILE_PATH, index=False)
