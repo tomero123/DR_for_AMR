c6bc3316fa7a7345c5def7226730f899382d18d0
TomerOrgad88 <tomerorgad@gmail.com>
2019-11-19 14:55:00 +0200

    --

--- ncbi_data/train_and_test_5.py
+++ ncbi_data/train_and_test_5.py
@@ -111,7 +111,7 @@ def train_test_and_write_results_cv(final_df, results_file_path, model, model_pa
         selection_model = xgboost.XGBClassifier(random_state=random_seed)
         selection_model.set_params(**model_params)
         # cross validation using selection_model
-        print("Started running Cross Validation for {} folds with {} processes".format(k_folds, num_of_processes))
+        print("Started running Cross Validation for {} folds with {} processes ; X.shape: {}".format(k_folds, num_of_processes, str(select_X.shape)))
         now = time.time()
         cv = StratifiedKFold(k_folds, random_state=random_seed, shuffle=True)
         classes = np.unique(y.values.ravel())
