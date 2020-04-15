03a7ec011115ba284f346466a510bbc145276e5e
TomerOrgad88 <tomerorgad@gmail.com>
2020-04-15 10:50:35 +0300

    --

--- doc2vec/doc2vec_train_2.py
+++ doc2vec/doc2vec_train_2.py
@@ -33,6 +33,6 @@ if __name__ == '__main__':
     #
 
     model_save_name = get_file_name("", "model")
-    trainer = Doc2VecTrainer(input_folder, models_folder, files_list[:5], model_save_name)
+    trainer = Doc2VecTrainer(input_folder, models_folder, files_list, model_save_name)
     trainer.run()
     print(f"Finished training for bacteria: {BACTERIA} processing mode: {PROCESSING_MODE} shift size: {SHIFT_SIZE} in {round((time.time() - now) / 60, 4)} minutes")
