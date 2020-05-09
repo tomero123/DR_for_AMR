3d51e6c4ad6b296286ba93834d67cacc48ed35ee
TomerOrgad88 <tomerorgad@gmail.com>
2020-05-09 10:29:27 +0300

    --

--- classic_ml/download_assembly_status_1.py
+++ classic_ml/download_assembly_status_1.py
@@ -11,8 +11,8 @@ from classic_ml.utils import open_ftp_file
 # PARAMS
 
 BACTERIA = "genome_mix" if len(sys.argv) < 2 else sys.argv[1]
-NUM_OF_PROCESSES = 8
-limit = None  # if None - take all files found else limit
+NUM_OF_PROCESSES = 1
+limit = 100  # if None - take all files found else limit
 input_file_name = "genome_mix.csv"
 output_file_name = "{}_data.csv".format(BACTERIA)
 
@@ -24,7 +24,7 @@ FTP_FILE_NAME = "assembly_status.txt"
 PREFIX = '..' if os.name == 'nt' else '.'
 PATH = os.path.join(PREFIX, "data_files", BACTERIA)
 CSV_INPUT_FILE_PATH = os.path.join(PATH, input_file_name)
-# CSV_OUTPUT_FILE_PATH = os.path.join(PATH, output_file_name)
+CSV_OUTPUT_FILE_PATH = os.path.join(PATH, output_file_name)
 
 # PARAMS END
 
@@ -59,8 +59,8 @@ if __name__ == '__main__':
             df.at[ind, 'strain_validation'] = cur_strain_validation
             df.at[ind, 'status'] = cur_status
             if ind % 10 == 0:
-                df.to_csv(CSV_INPUT_FILE_PATH, index=False)
+                df.to_csv(CSV_OUTPUT_FILE_PATH, index=False)
                 print(f"Saved DF after {ind} iterations")
     # print(status_list)
-    df.to_csv(CSV_INPUT_FILE_PATH, index=False)
+    df.to_csv(CSV_OUTPUT_FILE_PATH, index=False)
     print("DONE!")
