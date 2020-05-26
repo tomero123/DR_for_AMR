492bab32c90e2290fa239447e4cfa371e4001349
TomerOrgad88 <tomerorgad@gmail.com>
2020-05-26 21:58:37 +0300

    --

--- enums.py
+++ enums.py
@@ -5,6 +5,7 @@ class Bacteria(Enum):
     MYCOBACTERIUM_TUBERCULOSIS = "mycobacterium_tuberculosis"
     PSEUDOMONAS_AUREGINOSA = "pseudomonas_aureginosa"
     GENOME_MIX = "genome_mix"
+    GENOME_MIX_NEW = "genome_mix_new"
 
 
 class ProcessingMode(Enum):
