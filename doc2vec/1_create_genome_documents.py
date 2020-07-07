f0419d2a0b3038eebc74b5cb56efb1dd37ccd6ee
TomerOrgad88 <tomerorgad@gmail.com>
2020-07-07 21:35:24 +0300

    --

--- doc2vec/1_create_genome_documents.py
+++ doc2vec/1_create_genome_documents.py
@@ -8,11 +8,11 @@ import gzip
 from functools import partial
 
 from utils import create_genome_document
-from enums import Bacteria
+from enums import Bacteria, ProcessingMode
 
 # PARAMS
-BACTERIA = Bacteria.PSEUDOMONAS_AUREGINOSA.value if len(sys.argv) <= 1 else sys.argv[1]
-PROCESSING_MODE = "overlapping" if len(sys.argv) <= 2 else sys.argv[2]  # can be "non_overlapping" or "overlapping"
+BACTERIA = Bacteria.GENOME_MIX_NEW.value if len(sys.argv) <= 1 else sys.argv[1]
+PROCESSING_MODE = ProcessingMode.OVERLAPPING.value if len(sys.argv) <= 2 else sys.argv[2]  # can be "non_overlapping" or "overlapping"
 K = 3 if len(sys.argv) <= 3 else int(sys.argv[3])  # Choose K size
 NUM_OF_PROCESSES = 10 if len(sys.argv) <= 4 else int(sys.argv[4])
 SHIFT_SIZE = 1  # relevant only for PROCESSING_MODE "overlapping"
