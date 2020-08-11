9ec3949719f559a6389a0e4dc7cef36dbddb795b
TomerOrgad88 <tomerorgad@gmail.com>
2020-08-11 22:12:58 +0300

    --

---
+++ doc2vec_cds/check.py
@@ -0,0 +1,31 @@
+import os
+import gensim
+import smart_open
+import time
+
+# Set file names for train and test data
+
+now = time.time()
+test_data_dir = os.path.join(gensim.__path__[0], 'test', 'test_data')
+lee_train_file = os.path.join(test_data_dir, 'lee_background.cor')
+lee_test_file = os.path.join(test_data_dir, 'lee.cor')
+
+def read_corpus(fname, tokens_only=False):
+    with smart_open.open(fname, encoding="iso-8859-1") as f:
+        for i, line in enumerate(f):
+            tokens = gensim.utils.simple_preprocess(line)
+            if tokens_only:
+                yield tokens
+            else:
+                # For training data, add tags
+                yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+train_corpus = list(read_corpus(lee_train_file))
+test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+
+model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+model.build_vocab(train_corpus)
+model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+vector = model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
+print(vector)
+print(f"DONE in {time.time() - now} seconds")
