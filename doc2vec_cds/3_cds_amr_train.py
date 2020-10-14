d79f1bfdd966fc760242400fe111112383cbad15
TomerOrgad88 <tomerorgad@gmail.com>
2020-10-14 09:27:22 +0300

    --

--- doc2vec_cds/3_cds_amr_train.py
+++ doc2vec_cds/3_cds_amr_train.py
@@ -54,7 +54,8 @@ if __name__ == '__main__':
         # "2020_10_09_1217_PM_overlapping_K_10_SS_1",
         # "2020_10_09_1217_PM_non_overlapping_K_10_SS_1"
         # "2020_09_28_1100_PM_overlapping_K_10_SS_10",
-        "2020_09_01_1931_PM_overlapping_K_10_SS_2"
+        # "2020_09_01_1931_PM_overlapping_K_10_SS_2"
+        "2020_10_13_1800_PM_overlapping_K_10_SS_5"
     ]
     # PARAMS END
     # IF RUNNING LOCAL (WINDOWS)
