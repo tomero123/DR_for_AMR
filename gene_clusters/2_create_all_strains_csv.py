0d3067d6daf7a7949206ec851aeb187de13e96f6
TomerOrgad88 <tomerorgad@gmail.com>
2020-11-03 14:33:44 +0200

    --

---
+++ gene_clusters/2_create_all_strains_csv.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.path.append("/home/local/BGU-USERS/tomeror/tomer_thesis")
+sys.path.append("/home/tomeror/tomer_thesis")
+
+import os
+import pandas as pd
+from pathos.multiprocessing import ProcessPool
+
+from gene_clusters.gene_clusters_utils import create_merged_file
+from constants import FileType, FILES_SUFFIX, Bacteria
+
+
+# PARAMS
+BACTERIA = Bacteria.PSEUDOMONAS_AUREGINOSA.value if len(sys.argv) <= 1 else sys.argv[1]
+NUM_OF_PROCESSES = 1 if len(sys.argv) <= 2 else int(sys.argv[2])
+
+prefix = '..' if os.name == 'nt' else '.'
+input_folder_path = os.path.join(prefix, "results_files", BACTERIA, "combined_genes_files")
+output_folder = os.path.join(prefix, "results_files", BACTERIA)
+
+
+files_list = os.listdir(input_folder_path)
+files_list = [x for x in files_list if ".csv.gz" in x]
+print(f"Number of combined files: {len(files_list)}")
+
+results_df = pd.DataFrame(columns=["file_name", "file", "number_of_genes"])
+
+for file in files_list:
+    df = pd.read_csv(os.path.join(input_folder_path, file))
+    results_df.loc[len(results_df)] = [file, file.replace(".csv.gz", ""), df.shape[0]]
+
+results_df = results_df.reset_index()
+results_df.to_csv(os.path.join(output_folder, "ALL_STRAINS.csv"), index=False)
--- gene_clusters/gene_clusters_utils.py
+++ gene_clusters/gene_clusters_utils.py
@@ -1,6 +1,7 @@
 import gzip
 import os
 import re
+import traceback
 from functools import partial
 
 import pandas as pd
@@ -12,50 +13,46 @@ _open = partial(gzip.open, mode='rt')
 
 
 def create_merged_file(input_list):
-    ind = input_list[0]
-    file = input_list[1]
-    input_folder_base = input_list[2]
-    output_folder = input_list[3]
+    try:
+        ind = input_list[0]
+        file = input_list[1]
+        input_folder_base = input_list[2]
+        output_folder = input_list[3]
 
-    features_table_file_path = os.path.join(input_folder_base, f"{FileType.FEATURE_TABLE.value}_files", file + "_" + FileType.FEATURE_TABLE.value + FILES_SUFFIX.get(FileType.FEATURE_TABLE.value))
-    protein_file_path = os.path.join(input_folder_base, f"{FileType.PROTEIN.value}_files", file + "_" + FileType.PROTEIN.value + FILES_SUFFIX.get(FileType.PROTEIN.value))
-    cds_file_path = os.path.join(input_folder_base, f"{FileType.CDS_FROM_GENOMIC.value}_files", file + "_" + FileType.CDS_FROM_GENOMIC.value + FILES_SUFFIX.get(FileType.CDS_FROM_GENOMIC.value))
+        features_table_file_path = os.path.join(input_folder_base, f"{FileType.FEATURE_TABLE.value}_files", file + "_" + FileType.FEATURE_TABLE.value + FILES_SUFFIX.get(FileType.FEATURE_TABLE.value))
+        protein_file_path = os.path.join(input_folder_base, f"{FileType.PROTEIN.value}_files", file + "_" + FileType.PROTEIN.value + FILES_SUFFIX.get(FileType.PROTEIN.value))
+        cds_file_path = os.path.join(input_folder_base, f"{FileType.CDS_FROM_GENOMIC.value}_files", file + "_" + FileType.CDS_FROM_GENOMIC.value + FILES_SUFFIX.get(FileType.CDS_FROM_GENOMIC.value))
 
-    output_path = os.path.join(output_folder, file + ".csv.gz")
+        output_path = os.path.join(output_folder, file + ".csv.gz")
 
-    features_data = pd.read_csv(features_table_file_path, compression='gzip', delimiter='\t')
+        features_data = pd.read_csv(features_table_file_path, compression='gzip', delimiter='\t')
 
-    # genes
-    gene = features_data[((features_data["# feature"] == 'gene') & (features_data["class"] == 'protein_coding'))]
-    cds = features_data[((features_data["# feature"] == 'CDS') & (features_data["class"] == 'with_protein'))]
-    gene_join = gene.merge(cds, on='locus_tag')
+        # genes
+        gene = features_data[((features_data["# feature"] == 'gene') & (features_data["class"] == 'protein_coding'))]
+        cds = features_data[((features_data["# feature"] == 'CDS') & (features_data["class"] == 'with_protein'))]
+        gene_join = gene.merge(cds, on='locus_tag')
 
-    # pseudogenes
-    # pseudo_gene = features_data[((features_data["# feature"] == 'gene') & (features_data["class"] == 'pseudogene'))]
-    # pseudo_cds = features_data[((features_data["# feature"] == 'CDS') & (features_data["class"] == 'without_protein'))]
-    # pseudo_join = pseudo_gene.merge(pseudo_cds, on='locus_tag')
+        # proteins
+        protein_df = pd.DataFrame(columns=['id', 'protein'])
+        for record in SeqIO.parse(_open(os.path.join(protein_file_path)), 'fasta'):
+            protein_df.loc[len(protein_df)] = [record.id, record.seq]
 
-    # proteins
-    protein_df = pd.DataFrame(columns=['id', 'protein'])
-    for record in SeqIO.parse(_open(os.path.join(protein_file_path)), 'fasta'):
-        protein_df.loc[len(protein_df)] = [record.id, record.seq]
+        # cds
+        dna_df = pd.DataFrame(columns=['locus_tag', 'dna'])
+        for record in SeqIO.parse(_open(os.path.join(cds_file_path)), 'fasta'):
+            try:
+                locus_tag = re.findall('\[locus_tag=(.+?)\]', record.description)[0]
+            except AttributeError:
+                locus_tag = ''
+            dna_df.loc[len(dna_df)] = [locus_tag, record.seq]
 
-    # cds
-    dna_df = pd.DataFrame(columns=['locus_tag', 'dna'])
-    for record in SeqIO.parse(_open(os.path.join(cds_file_path)), 'fasta'):
-        try:
-            locus_tag = re.findall('\[locus_tag=(.+?)\]', record.description)[0]
-        except AttributeError:
-            locus_tag = ''
-        dna_df.loc[len(dna_df)] = [locus_tag, record.seq]
+        # create genes merged file
+        gene_join = gene_join.merge(dna_df, on='locus_tag')
+        gene_join = gene_join.merge(protein_df, left_on='product_accession_y', right_on='id')
+        gene_join.to_csv(output_path, compression='gzip')
 
-    # create genes merged file
-    gene_join = gene_join.merge(dna_df, on='locus_tag')
-    gene_join = gene_join.merge(protein_df, left_on='product_accession_y', right_on='id')
-    gene_join.to_csv(output_path, compression='gzip')
+        print(f"Finished processing file #{ind + 1}: {file}")
 
-    print(f"Finished processing file #{ind + 1}: {file}")
-
-    # create pseudogenes merged file
-    # pseudo_join = pseudo_join.merge(dna_df, on='locus_tag')
-    # pseudo_join.to_csv("pseudo_genes.csv")
+    except Exception() as e:
+        print(f"ERROR at create_merged_file for: {file}, index: {ind}, message: {e}")
+        traceback.print_exc()
