004b9337ab5b908b614c996ac68a9906b2322439
TomerOrgad88 <tomerorgad@gmail.com>
2020-11-09 17:37:55 +0200

    --

--- doc2vec_cds/3_cds_amr_train.py
+++ doc2vec_cds/3_cds_amr_train.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     ]
     # Define list of model_names and processing method
     D2V_MODELS_LIST = [
-        "2020_11_07_2316_PM_overlapping_K_10_SS_1",
+        # "2020_11_07_2316_PM_overlapping_K_10_SS_1",
         "2020_11_07_2316_PM_non_overlapping_K_10_SS_1"
     ]
     # PARAMS END
