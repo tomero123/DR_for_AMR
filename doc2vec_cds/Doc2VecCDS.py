a966ae37a99040ce6c5b1037ea30f2debcf60427
TomerOrgad88 <tomerorgad@gmail.com>
2020-11-11 10:16:13 +0200

    --

--- doc2vec_cds/Doc2VecCDS.py
+++ doc2vec_cds/Doc2VecCDS.py
@@ -120,7 +120,7 @@ class Doc2VecCDS(object):
         min_count = 20
         sample = 1e-4
         negative = 5
-        epochs = 10
+        epochs = 20
         # PARAMS END
 
         self.conf_dict["vector_size"] = vector_size
